f0abab9c1a5f6998bad7249ddbab82d784fc8d89
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2020-05-06 16:24:02 +0200

    end

--- app/add-element/add-element.js
+++ app/add-element/add-element.js
@@ -1,3 +1,6 @@
+const { ipcRenderer } = require("electron");
+const remote = require('electron').remote;
+
 const formInput = document.getElementById('form-input')
 const formType = document.getElementById('form-type')
 const formSend = document.getElementById('form-send')
@@ -7,4 +10,39 @@ formSend.addEventListener('click', function() {
 
     alert('Vous avez créé le ' + formType.value + ' nommé ' + formInput.value )
 
+    // Créez la variable "element" qui contient un objet, avec les données de l'utilisateur
+
+    let element = {
+        title: formInput.value,
+        type: formType.value
+    }
+
+    // Créez le tableau "elementsArray", vide, pour avoir par défaut un tableau
+    // d'éléments vide que l'on déclare
+    let elementsArray = [];
+
+    // Vérifiez si la clé "elements" existe dans le localStorage. Pour cela :
+    if ( localStorage.getItem('elements') !== null  ) {
+
+        // Si le tableau "elements" existe dans localStorage, alors on l'assigne à elementsArray
+        // pour récupérer les éléments existants
+        elementsArray = JSON.parse(localStorage.getItem('elements'))
+    }
+
+    // Ajoutez l'élément utilisateur (variable "element") au tableau "elementsArray"
+    // grâce à push() : tableau.push(element_a_rajouter)
+    elementsArray.push(element)
+
+    // Enfin, enregistrez en version strigifiée 
+
+    localStorage.setItem("elements", JSON.stringify(elementsArray));
+
+    remote.getCurrentWindow().getParentWindow().send('element-added');
+})
+
+
+const btnSendMessage = document.getElementById('btn-send-message')
+
+btnSendMessage.addEventListener('click', function() {
+    remote.getCurrentWindow().getParentWindow().send('clicked-button');
 })
--- app/index/index.js
+++ app/index/index.js
@@ -1,16 +1,65 @@
+const remote = require('electron').remote;
 const { BrowserWindow } = require('electron').remote;
+const { ipcRenderer } = require('electron');
+
+let elementsArray = JSON.parse(localStorage.getItem('elements'));
+
+const elementsList = document.getElementById('elements-list');
+elementsList.innerHTML = '';
+
+elementsArray.forEach(function (element, index) {
+    let elementHtml = '<li><strong>' + element.type + '</strong> - ' + element.title + '</li>';
+    elementsList.innerHTML += elementHtml;
+})
 
 const btnAddElement = document.getElementById('btn-add-element');
 
 btnAddElement.addEventListener('click', () => {
 
-    const winIndex = new BrowserWindow({
+    const winAddElement = new BrowserWindow({
         width: 800,
-        height: 600,    
+        height: 600,
+        parent: remote.getCurrentWindow(),
         webPreferences: {
             nodeIntegration: true
         }
     });
 
-    winIndex.loadFile('app/add-element/add-element.html');
+    winAddElement.loadFile('app/add-element/add-element.html');
+    winAddElement.webContents.send('ok')
+});
+
+ipcRenderer.on('clicked-button', function() {
+    console.log('Le bouton a été cliqué depuis la fenêtre enfant !');
+});
+
+ipcRenderer.on('element-added', function () {
+    console.log('Un élément a été ajouté au localStorage !')
+
+    // 1. On récupère les données depuis le localStorage, et on les parse
+    let elementsArray = JSON.parse(localStorage.getItem('elements'))
+
+    // 2. On fait une boucle forEach qui permet de scanner le tableau
+    // et faire un console.log de chaque élément
+    elementsArray.forEach(function(element, index) {
+        console.log(element.type + " " + element.title)
+    })
+
+    // 3. On gère l'affichage dans le HTML
+
+    // On récupère la balise HTML qui contiendra mes éléments
+    const elementsList = document.getElementById('elements-list')
+
+    // D'abord, on vide l'affichage existant si jamais il y avaitdéjà une liste affichée dans elementsList
+    elementsList.innerHTML = '';
+
+    // Ensuite, on fait une boucle sur tous les éléments
+    elementsArray.forEach(function(element, index) {
+
+        // On prépare le code HTML d'un élément de la liste
+        let elementHtml = '<li><strong>'+ element.type +'</strong> - '+ element.title +'</li>';
+
+        // Ensuite, on ajoute au sein du HTML de notre balise elementsList, l'élément à rajouter :
+        elementsList.innerHTML += elementHtml;
+    })
 });
